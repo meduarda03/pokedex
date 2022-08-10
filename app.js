d673eda5196503b5aa12a1ec156480756dfbbb88
Maria Eduarda <mariaeduarda53992@gamil.com>
2022-08-10 18:48:03 -0300

    inicio do projeto

---
+++ app.js
@@ -0,0 +1,13 @@
+const fetchPokemon = () = > {
+  const getPokemonUrl = id = > ' https://pokeapi.co/api/v2/pokemon/${id}'
+
+    const pokemonPromises = []
+
+  for (let i = 1; i < = 150; i++) {
+    pokemonPromises.push(fetch(getPokemonUrl(i)).then(response = > response.json()))
+    }
+
+    
+}
+
+fetchPokemon()
